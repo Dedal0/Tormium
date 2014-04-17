5ef2c6c5bed8e98bb438f8fee8e047b2bb32fd01
Camilo Galdos AkA Dedalo <seguridadblanca@gmail.com>
2014-04-17 13:12:17 -0500

    Update tormium.py
    Stable version with better privacy.

--- tormium.py
+++ tormium.py
@@ -2,11 +2,11 @@
 
 #Written By Dedalo (@SeguridadBlanca)
 
-import sys
 import commands
 
 end = "\033[0m"
 red = "\033[1;91m"
+green = "\033[92m"
 
 #verify if tor running
 output = commands.getoutput('ps -A')
@@ -17,9 +17,20 @@ port = 9050
 
 #Execute
 def executetor():
+	print green + '''
+ _______                   _                 
+|__   __|                 (_)                
+   | | ___  _ __ _ __ ___  _ _   _ _ __ ___  
+   | |/ _ \| '__| '_ ` _ \| | | | | '_ ` _ \ 
+   | | (_) | |  | | | | | | | |_| | | | | | |
+   |_|\___/|_|  |_| |_| |_|_|\__,_|_| |_| |_|
+    Welcome To Tormium                                         
+''' + end
+
 	ruta = "tor/.config/chromium/Tormium"
-	exetor = "chromium-browser --user-data-dir=\"" + ruta + "\" --proxy-server=\"socks5://127.0.0.1:" + str(port) + "\" --incognito check.torproject.org"
+	exetor = "chromium-browser --user-data-dir=\"" + ruta + "\" --proxy-server=\"socks5://127.0.0.1:" + str(port) + "\" --no-referrers --user-agent=\"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0\" --disable-translate --disable-geolocation --dns-prefetch-disable --no-pings --disable-javascript check.torproject.org"
 	commands.getoutput(exetor)
+	commands.getoutput("rm -rf tor/")
 
 
 #Goodbye
